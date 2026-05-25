5a60803c99064c9bc499b21f67e231e35b99bd14
Shin Kojima <shin@kojima.org>
2026-05-26 01:03:14 +0900

    fix using tokenizer.awk

--- to_json.sh
+++ to_json.sh
@@ -7,7 +7,7 @@ dir=$(cd "$(dirname "$0")" && pwd)
 # Please see `Working with arrays` section in
 # Rich's sh (POSIX shell) tricks:
 # http://www.etalabs.net/sh_tricks.html
-eval "set -- $(sh "$dir/parser.sh")"
+eval "set -- $(awk -f "$dir/src/tokenizer.awk")"
 
 # Each odd index elements indicate token name
 # Each even index elements contains token value
